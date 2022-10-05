f0ef75fdc678d01c9edf39d7673c02a59ea11cc2
NubeDev <aidan@enviroservices.com.au>
2022-10-05 19:03:15 +1100

    bump flow-eng version

--- flow/flow.go
+++ flow/flow.go
@@ -4,8 +4,12 @@ import (
 	"fmt"
 	flowctrl "github.com/NubeDev/flow-eng"
 	"github.com/NubeDev/flow-eng/db"
+	"github.com/NubeDev/flow-eng/helpers/names"
 	"github.com/NubeDev/flow-eng/node"
 	"github.com/NubeDev/flow-eng/nodes"
+	"github.com/NubeDev/flow-eng/nodes/protocols/bacnet"
+	"github.com/NubeDev/flow-eng/nodes/protocols/bacnet/points"
+	"github.com/NubeDev/flow-eng/services/mqttclient"
 	log "github.com/sirupsen/logrus"
 	"time"
 )
@@ -43,12 +47,30 @@ type Message struct {
 func loop() {
 
 	var nodesList []node.Node
+	mqttClient, err := mqttclient.NewClient(mqttclient.ClientOptions{
+		Servers: []string{"tcp://0.0.0.0:1883"},
+	})
+	err = mqttClient.Connect()
+	if err != nil {
+		log.Error(err)
+	}
 
+	opts := &bacnet.Bacnet{
+		Store:       points.New(names.Edge, nil, 0, 200, 200),
+		MqttClient:  mqttClient,
+		Application: names.Edge,
+	}
 	for _, n := range latestFlow {
-		newNode, err := nodes.Builder(n, storage)
+		var node_ node.Node
+		if n.Info.Category == "bacnet" {
+			node_, err = nodes.Builder(n, storage, opts)
+		} else {
+			node_, err = nodes.Builder(n, storage)
+		}
+
 		if err != nil {
 		}
-		nodesList = append(nodesList, newNode)
+		nodesList = append(nodesList, node_)
 	}
 	flowInst.AddNodes(nodesList...)
 	flowInst.ReBuildFlow(true)
