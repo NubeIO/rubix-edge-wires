8e331dcacf12353be62b5d1f0db199f36e9d2997
NubeDev <aidan@enviroservices.com.au>
2023-01-12 13:49:47 +1100

    bump flow version and added default args for cobra

--- cmd/root.go
+++ cmd/root.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"github.com/spf13/cobra"
+	"os"
 )
 
 // RootCmd represents the base command when called without any subcommands
@@ -14,7 +15,13 @@ var RootCmd = &cobra.Command{
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
+	cmd, _, err := RootCmd.Find(os.Args[1:])
+	if err == nil && cmd.Use == RootCmd.Use {
+		args := append([]string{"server", "-auth=false"}, os.Args[1:]...)
+		RootCmd.SetArgs(args)
+	}
 	if err := RootCmd.Execute(); err != nil {
+
 	}
 }
 
@@ -30,7 +37,7 @@ var flgRoot struct {
 
 func init() {
 	RootCmd.PersistentFlags().BoolVarP(&flgRoot.prod, "prod", "", false, "prod")
-	RootCmd.PersistentFlags().BoolVarP(&flgRoot.auth, "auth", "", true, "auth")
+	RootCmd.PersistentFlags().BoolVarP(&flgRoot.auth, "auth", "", false, "auth")
 	RootCmd.PersistentFlags().IntVarP(&flgRoot.port, "port", "p", 1665, "port (default 1665)")
 	RootCmd.PersistentFlags().StringVarP(&flgRoot.rootDir, "root-dir", "r", "./", "root dir") // in production, it will be `/data`
 	RootCmd.PersistentFlags().StringVarP(&flgRoot.appDir, "app-dir", "a", "./", "app dir")    // in production, it will be `rubix-edge-wires`
